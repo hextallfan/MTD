0f92ce9f3a7d7f364d912683ea0cd601679648dc
hextallfan <46456449+hextallfan@users.noreply.github.com>
2019-04-12 20:25:53 -0700

    Add files via upload

---
+++ mexicantraindominos-hextallfan-master/MTD/MTDClasses/BoneYard.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTDClasses
+{
+    public class BoneYard
+    {
+        private List<Domino> listOfDominos = new List<Domino>();
+        
+        //create maxDots for total number of domino values so you can play other games that require different domino values
+        public BoneYard(int maxDots)
+        {
+            for (int side1 = 0; side1 <= maxDots; side1++)
+            {
+                for (int side2 = side1; side2 <= maxDots; side2++)
+                {
+                    Domino d = new Domino(side1, side2);
+                    listOfDominos.Add(d);
+                }
+            }
+        }
+
+        //Shuffle the dominos in the Boneyard so you get a random domino upon drawing
+        public void Shuffle()
+        {
+            Random gen = new Random();
+            for (int i = 0; i < DominosRemaining; i++)
+            {
+                int rnd = gen.Next(i, DominosRemaining);
+                Domino d = listOfDominos[rnd];
+                listOfDominos[rnd] = listOfDominos[i];
+                listOfDominos[i] = d;
+            }
+        }
+
+        //Test if the boneyard has no more reamining dominos
+        public bool IsEmpty()
+        {
+            if (DominosRemaining == 0)
+                return true;
+            else
+                return false;
+        }
+
+        //return the total count of dominos in the boneyard
+        public int DominosRemaining
+        {
+            get { return listOfDominos.Count; }
+        }
+
+        //draw a domino if there are dominos remaining in the boneyard
+        public Domino Draw()
+        {
+            Domino drawDomino = null;
+            if (IsEmpty() == false)
+            {
+                drawDomino = listOfDominos[0];
+                listOfDominos.RemoveAt(0);
+            }
+            return drawDomino;
+        }
+
+        //Create an index to hold a domino value
+        public Domino this[int index]
+        {
+            get
+            {
+                if (index < 0)
+                {
+                    throw new ArgumentOutOfRangeException("index");
+                }
+                else if (index >= listOfDominos.Count)
+                {
+                    throw new ArgumentOutOfRangeException("index");
+                }
+                return listOfDominos[index];
+            }
+            set
+            {
+                listOfDominos[index] = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            foreach (Domino d in listOfDominos)
+            {
+                str += (d.ToString() + "n");
+            }
+            return str;
+        }
+        
+    }
+}
---
+++ mexicantraindominos-hextallfan-master/MTD/MTDTests/BoneyardTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using MTDClasses;
+
+namespace MTDTests
+{
+    [TestFixture]
+    public class BoneyardTest
+    {
+        [Test]
+        public void TestConstructor()
+        {
+            //Test the constructors
+            BoneYard b6 = new BoneYard(6);
+            Assert.AreEqual(28, b6.DominosRemaining);
+            Assert.AreEqual(new Domino(0, 0), b6[0]);
+            Assert.AreEqual(new Domino(6, 6), b6[27]);
+            Assert.AreEqual(new Domino(1, 1), b6[7]);
+        }
+
+        [Test]
+        public void TestBoneYardShuffle()
+        {
+            //Test my shuffle by drawing a domino and then shuffling and the drawn domino is not the first domino created  
+            BoneYard b = new BoneYard(12);
+            Assert.AreEqual(new Domino(0, 0), b.Draw());
+            b.Shuffle();
+            Assert.False(new Domino(0, 0) == b.Draw());
+        }
+
+        [Test]
+        public void TestBoneYardIsEmpty()
+        {
+            //Test that the boneyard is not empty
+            BoneYard d = new BoneYard(12);
+            Assert.False(d.IsEmpty());
+        }
+
+        [Test]
+        public void TestBoneYardDraw()
+        {
+            //Testing if Draw() gets a return domino
+            BoneYard b = new BoneYard(12);
+            Assert.AreEqual(new Domino(0, 0), b.Draw());
+        }
+
+        [Test]
+        public void TestBoneYardDominosRemaining()
+        {
+            //Testing if the DominosRemaining count returns a positive integer
+            Assert.True(DominosRemaning.Count() >= 0);
+        }
+        [Test]
+        public void TestBoneYardToString()
+        {
+            //Testing is return of ToString is a string
+            BoneYard b = new BoneYard(12);
+            Assert.True(b.Tostring());
+        }
+    }
+}
